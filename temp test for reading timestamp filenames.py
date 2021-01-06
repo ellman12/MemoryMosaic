2751852e5e8c2d04ce8dc48ec2919acf3c8f3029
Elliott DuCharme <ellduc4@gmail.com>
2021-01-05 20:28:00 -0600

    add comment

--- temp test for reading timestamp filenames.py
+++ temp test for reading timestamp filenames.py
@@ -5,5 +5,6 @@ filename = "Screenshot_20201028-141626_Messages.jpg"
 stripped_timestamp = filename[11:19] + filename[21:26] # Strip the chars we don't want.
 print(stripped_timestamp)
 
+# https://stackoverflow.com/questions/2380013/converting-date-time-in-yyyymmdd-hhmmss-format-to-python-datetime
 dt=datetime.datetime.strptime(stripped_timestamp,'%Y%m%d%H%M%S')
 print(dt)
