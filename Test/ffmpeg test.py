0bd913d7d102ec2e558ff3b3963d41bdb7404db8
Elliott DuCharme <ellduc4@gmail.com>
2021-01-11 12:56:48 -0600

    I think this test script works now.

--- Test/ffmpeg test.py
+++ Test/ffmpeg test.py
@@ -1,7 +1,7 @@
+# Test for generating a sorted dir for an mp4 file using ffmpeg.
 import subprocess
 import dateutil.parser
 import datetime
-import ffmpeg
 
 def readVideoMetadata(filename):
     results = subprocess.run(["ffprobe", "-v", "0", "-print_format", "compact=print_section=0:nk=1", "-show_entries", "format_tags=creation_time", filename], stdout = subprocess.PIPE)
@@ -9,9 +9,11 @@ def readVideoMetadata(filename):
     video_datetime = dateutil.parser.isoparse(output)
     return video_datetime
 
-FILENAME = "C:\\Users\\Elliott\\Documents\\GitHub\\Photos-Storage-Server\\Photos\\VID_20201126_121225.mp4"
+FILENAME = "C:/Users/Elliott/Documents/GitHub/Photos-Storage-Server/Photos/VID_20201126_121225.mp4"
 
 test = readVideoMetadata(FILENAME)
-test = datetime.datetime.strptime(str(test), "%Y:%m:%d %H:%M:%S")
+
+newDir = "C:/Users/Elliott/Documents/GitHub/Photos-Storage-Server/" + str(test.year) + '/' + str(test.month) + '/' + "VID_20201126_121225.mp4"
 
 print(test)
+print(newDir)
