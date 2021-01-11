eab8057c7dde016b7972006e9730976364012801
Elliott DuCharme <ellduc4@gmail.com>
2021-01-10 20:33:13 -0600

    trying to get ffmpeg working... :/

--- Test/ffmpeg test.py
+++ Test/ffmpeg test.py
@@ -1,7 +1,7 @@
 import subprocess
 import dateutil.parser
 import datetime
-# import ffmpeg
+import ffmpeg
 
 def readVideoMetadata(filename):
     results = subprocess.run(["ffprobe", "-v", "0", "-print_format", "compact=print_section=0:nk=1", "-show_entries", "format_tags=creation_time", filename], stdout = subprocess.PIPE)
@@ -9,7 +9,7 @@ def readVideoMetadata(filename):
     video_datetime = dateutil.parser.isoparse(output)
     return video_datetime
 
-FILENAME = "C:/Users/Elliott/Documents/GitHub/Photos-Storage-Server/Photos/29e5607b885099d7ba2d3fbb88e3328a9ba92ecc~2.mp4"
+FILENAME = "C:\\Users\\Elliott\\Documents\\GitHub\\Photos-Storage-Server\\Photos\\VID_20201126_121225.mp4"
 
 test = readVideoMetadata(FILENAME)
 test = datetime.datetime.strptime(str(test), "%Y:%m:%d %H:%M:%S")
