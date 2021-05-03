a26e15eb571680b9aaf6da023ff4d5dd3ec3a6b3
Elliott DuCharme <ellduc4@gmail.com>
2021-05-03 14:56:50 -0500

    GetFilenameTimestamp can strip filenames. Onto datetime conversion

--- PSS Photo Sorter/Program.cs
+++ PSS Photo Sorter/Program.cs
@@ -9,7 +9,8 @@ namespace PSS_Photo_Sorter
             DateTime date = PSSPhotoSorter.GetVidDate("C:/Users/Elliott/Documents/GitHub/Photos-Storage-Server/Pics and Vids/Test Vids/20210501_193046.mp4");
             Console.WriteLine(date);
 
-            PSSPhotoSorter.GetFilenameTimestamp("C:/Users/Elliott/Documents/GitHub/Photos-Storage-Server/Pics and Vids/Test Vids/", "20210501_193046.mp4");
+            string yes = PSSPhotoSorter.GetFilenameTimestamp("C:/Users/Elliott/Documents/GitHub/Photos-Storage-Server/Pics and Vids/Test Vids/", "Screenshot 2020-11-");
+            Console.WriteLine(yes);
         }
     }
 }
