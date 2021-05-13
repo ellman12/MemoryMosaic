1ddb242d38ffb669edffc884c80a4fa999238421
Elliott DuCharme <ellduc4@gmail.com>
2021-05-13 13:51:39 -0500

    create CreateTables for initializing the DB

---
+++ Actual DB Test/Actual DB Test/CreateTables.sql
@@ -0,0 +1,25 @@
+CREATE TABLE `album_entries` (
+  `path` varchar(600) NOT NULL,
+  `album_id` int unsigned NOT NULL,
+  `date_added_to_album` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `albums` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(600) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `media` (
+  `path` varchar(600) NOT NULL,
+  `date_added` datetime NOT NULL,
+  `date_taken` datetime NOT NULL,
+  `Separate` tinyint(1) NOT NULL,
+  PRIMARY KEY (`path`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `trash` (
+  `path` varchar(600) NOT NULL,
+  `album_id` int unsigned NOT NULL,
+  `date_added_to_album` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
