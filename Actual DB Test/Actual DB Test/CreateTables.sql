533a86387544aa7f6aee8fd0e0bfafeaf577b950
Elliott DuCharme <ellduc4@gmail.com>
2021-05-17 22:09:41 -0500

    add date_taken to album_entries

--- Actual DB Test/Actual DB Test/CreateTables.sql
+++ Actual DB Test/Actual DB Test/CreateTables.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `album_entries` (
   `path` varchar(600) NOT NULL,
   `album_id` int unsigned NOT NULL,
+  `date_taken` datetime NOT NULL,
   `date_added_to_album` datetime NOT NULL,
   PRIMARY KEY (`path`,`album_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -8,6 +9,7 @@ CREATE TABLE `album_entries` (
 CREATE TABLE `album_entries_trash` (
   `path` varchar(600) NOT NULL,
   `album_id` int unsigned NOT NULL,
+  `date_taken` datetime NOT NULL,
   `date_added_to_album` datetime NOT NULL,
   PRIMARY KEY (`path`,`album_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -24,7 +26,7 @@ CREATE TABLE `media` (
   `path` varchar(600) NOT NULL,
   `date_added` datetime NOT NULL,
   `date_taken` datetime NOT NULL,
-  `Separate` tinyint(1) NOT NULL,
+  `Separate` BOOLEAN NOT NULL,
   PRIMARY KEY (`path`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -32,6 +34,6 @@ CREATE TABLE `media_trash` (
   `path` varchar(600) NOT NULL,
   `date_added` datetime NOT NULL,
   `date_taken` datetime NOT NULL,
-  `Separate` tinyint(1) NOT NULL,
+  `Separate` BOOLEAN NOT NULL,
   PRIMARY KEY (`path`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
