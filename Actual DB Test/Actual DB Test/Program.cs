3b305264c993ab0d602d3d5a86a970779456aa51
Elliott DuCharme <ellduc4@gmail.com>
2021-07-22 14:48:28 -0500

    not working

--- Actual DB Test/Actual DB Test/Program.cs
+++ Actual DB Test/Actual DB Test/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using PSS.Client.Backend;
 
 namespace Actual_DB_Test
 {
@@ -8,14 +9,16 @@ namespace Actual_DB_Test
         static void Main()
         {
             Connection c = new();
-            c.OpenConnection();
+            //c.OpenConnection();
             //c.ClearTables();
             //c.MediaAndAlbumInsert("new item1", 5, DateTime.Now);
             //c.MediaAndAlbumInsert("new item2", 5, DateTime.Now);
             //c.MediaAndAlbumInsert("new item3", 5, DateTime.Now);
             //c.MediaAndAlbumInsert("new item4", 5, DateTime.Now);
             //c.DeleteItem("new item3");
-            c.RestoreItem("new item3");
+            //c.RestoreItem("new item3");
+            c.InsertMedia("path1", DateTime.MaxValue);
+            c.InsertMedia("path2", DateTime.MinValue);
             c.CloseConnection();
         }
     }
---
+++ PSS/Client/Backend/SQL Scripts/Test Script.sql
@@ -0,0 +1,31 @@
+INSERT INTO albums (Name) values ("test_album");
+
+select * from albums;
+select * from album_entries;
+select * from media;
+select * from album_entries_trash;
+select * from media_trash;
+
+UPDATE albums SET album_cover = "test lol" WHERE id = 2;
+UPDATE albums SET album_cover = null WHERE id = 2;
+
+UPDATE media SET path = "new path lol" WHERE path = "item1";
+UPDATE album_entries SET path = "new path lol" WHERE path = "item1";
+
+-- Works
+SELECT a.path, a.album_id, a.date_added_to_album, m.date_taken FROM media AS m
+INNER JOIN album_entries AS a
+ON m.path = a.path
+WHERE album_id=2;
+
+-- Also works
+SELECT a.path, m.date_taken, a.date_added_to_album FROM media AS m
+INNER JOIN album_entries AS a
+ON m.path = a.path
+WHERE album_id=1;
+
+-- Select only items from media that have 0 (false)
+select * from media where Separate = "0";
+
+-- Removing a single path from an album
+DELETE FROM album_entries WHERE album_id = 4 AND path = "item6";
