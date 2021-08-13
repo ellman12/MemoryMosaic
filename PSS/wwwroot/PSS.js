1aba6e94507d9bc085b8af63be2880a36569ac31
Elliott DuCharme <ellduc4@gmail.com>
2021-08-13 15:23:16 -0500

    add notImplemented() and cleanup Test page

--- PSS/wwwroot/PSS.js
+++ PSS/wwwroot/PSS.js
@@ -1,16 +1,4 @@
-﻿//Drop-down js for Photo Viewer: https://www.w3schools.com/howto/howto_js_dropdown.asp
-function toggleDropdown() { document.getElementById("dropdown").classList.toggle("show"); }
-
-//Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdownDiv");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
+﻿//If something like a button isn't implemented, have it call this to state that it isn't implemented.
+function notImplemented() {
+    window.alert("This item is not implemented yet :(");
 }
