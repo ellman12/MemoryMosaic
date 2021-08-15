e89a423ec873d63719281a733e16791aba7a6ad0
Elliott DuCharme <ellduc4@gmail.com>
2021-08-15 18:12:21 -0500

    Create GetFilenameFromUuid()

--- PSS/Backend/SQL Scripts/Test Script.sql
+++ PSS/Backend/SQL Scripts/Test Script.sql
@@ -31,3 +31,9 @@ select * from media where Separate = "0";
 DELETE FROM album_entries WHERE album_id = 4 AND path = "item6";
 
 SELECT path FROM media WHERE uuid="0d986559-fa29-11eb-93b1-ec79492ad08a";
+
+-- Get date_taken with uuid.
+SELECT date_taken FROM media WHERE uuid="0f9e7603-fde0-11eb-8f8c-1c1b0d0d5ac3";
+
+-- Get path with uuid.
+SELECT path FROM media WHERE uuid="abcdef";
