d9f04cdb2bedd9fc69170e2af395c6f55c5033d4
Elliott DuCharme <ellduc4@gmail.com>
2021-08-17 20:01:57 -0500

    controls can now be toggled by pushing pencil btn and disabled by pushing cancel btn

--- PSS/wwwroot/PSS.js
+++ PSS/wwwroot/PSS.js
@@ -2,3 +2,28 @@
 function notImplemented() {
     window.alert("This item is not implemented yet :(");
 }
+
+function enableControls() {
+    document.getElementById("year-edit").disabled = false;
+    document.getElementById("year-edit").focus();
+    document.getElementById("month-edit").disabled = false;
+    document.getElementById("day-edit").disabled = false;
+    document.getElementById("hour-edit").disabled = false;
+    document.getElementById("minute-edit").disabled = false;
+    document.getElementById("second-edit").disabled = false;
+    document.getElementById("period-btn").disabled = false;
+    document.getElementById("cancel-btn").disabled = false;
+    document.getElementById("apply-btn").disabled = false;
+}
+
+function disableControls() {
+    document.getElementById("year-edit").disabled = true;
+    document.getElementById("month-edit").disabled = true;
+    document.getElementById("day-edit").disabled = true;
+    document.getElementById("hour-edit").disabled = true;
+    document.getElementById("minute-edit").disabled = true;
+    document.getElementById("second-edit").disabled = true;
+    document.getElementById("period-btn").disabled = true;
+    document.getElementById("cancel-btn").disabled = true;
+    document.getElementById("apply-btn").disabled = true;
+}
