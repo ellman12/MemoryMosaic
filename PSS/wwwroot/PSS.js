bf1fefc2d3826c5dac97805c588501085b89368f
Elliott DuCharme <ellduc4@gmail.com>
2021-11-01 23:12:59 -0500

    Implement List to store Guids of items checked. Checking items should all be done now (now on to adding to albums, deleting, etc.)

--- PSS/wwwroot/PSS.js
+++ PSS/wwwroot/PSS.js
@@ -4,12 +4,17 @@ function notImplemented() {
 }
 
 function toggleCheck(id) {
+    let className;
     if (document.getElementById(id).classList.contains("unchecked")) {
         document.getElementById(id).classList.remove("unchecked");
         document.getElementById(id).classList.add("checked");
+        className = "checked";
 
     } else if (document.getElementById(id).classList.contains("checked")) {
         document.getElementById(id).classList.remove("checked");
         document.getElementById(id).classList.add("unchecked");
+        className = "unchecked";
     }
+    
+    return className;
 }
