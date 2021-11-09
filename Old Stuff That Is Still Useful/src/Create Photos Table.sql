b3b923f15f9e98cfac221d3f3c6b87326bbe0a1f
Elliott DuCharme <ellduc4@gmail.com>
2021-11-09 08:51:24 -0600

    Remove tons of old testing stuff that are no longer needed

--- Old Stuff That Is Still Useful/src/Create Photos Table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE photos (
-	Directory VARCHAR(200) NOT NULL UNIQUE PRIMARY KEY,
-    Date_Added DATETIME NOT NULL,
-    Date_Taken DATETIME,
-    Albums_List CHAR(75)
-);
--- Old Stuff That Is Still Useful/src/Functions/readVideoMetadata.py
+++
@@ -1,9 +0,0 @@
-# Pretty simple function that returns when a video file was taken using FFmpeg.
-import subprocess
-import dateutil.parser
-
-def readVideoMetadata(dir):
-    fileInfo = subprocess.run(["ffprobe", "-v", "0", "-print_format", "compact=print_section=0:nk=1", "-show_entries", "format_tags=creation_time", dir], stdout = subprocess.PIPE)
-    output = fileInfo.stdout.decode().strip()
-    videoDatetime = dateutil.parser.isoparse(output)
-    return videoDatetime
