d0056d44ff8e988be0a39563f7819d41989872d5
Elliott <ellduc4@gmail.com>
2021-12-23 15:30:20 -0600

    comment

--- PSS/Backend/Maintenance.cs
+++ PSS/Backend/Maintenance.cs
@@ -11,7 +11,7 @@ namespace PSS.Backend
     {
         /// <summary>
         /// Return if a folder is empty (has 0 files in folder or subfolders). Subfolders inside path aren't counted unless they have stuff inside.
-        ///  If root/ has 2 subfolders and 3 files it would only find 3 files and thus it wouldn't be counted as empty.
+        /// If root/ has 2 subfolders and 3 files it would only find 3 files and thus it wouldn't be counted as empty.
         /// If root/ has 2 subfolders and 0 files, it would be considered empty.
         /// </summary>
         /// <returns>True if empty.</returns>
