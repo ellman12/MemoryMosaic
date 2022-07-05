35040d780dd43d9e18d572203745ef6d30311c81
Elliott DuCharme <ellduc4@gmail.com>
2022-07-05 13:45:08 -0600

    Remove old kinda garbo JS stuff for getting ctrl or shift down. Won't need anymore

--- PSS/PSS/wwwroot/PSS.js
+++ PSS/PSS/wwwroot/PSS.js
@@ -1,6 +1,3 @@
-let ctrlDown = false;
-let shiftDown = false;
-
 function toggleCheck(id) {
     let className;
     if (document.getElementById(id).classList.contains("unchecked")) {
@@ -22,35 +19,3 @@ function removeCheck(id) {
     document.getElementById(id).classList.remove("checked");
     document.getElementById(id).classList.add("unchecked");
 }
-
-function getCtrlDown() {
-    return ctrlDown;
-}
-
-function getShiftDown() {
-    return shiftDown;
-}
-
-window.addEventListener("keydown", function(event) {
-    if (event.code === "ControlLeft" || event.code === "ControlRight") {
-        ctrlDown = true;
-    }
-}, true);
-
-window.addEventListener("keyup", function(event) {
-    if (event.code === "ControlLeft" || event.code === "ControlRight") {
-        ctrlDown = false;
-    }
-}, true);
-
-window.addEventListener("keydown", function(event) {
-    if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
-        shiftDown = true;
-    }
-}, true);
-
-window.addEventListener("keyup", function(event) {
-    if (event.code === "ShiftLeft" || event.code === "ShiftRight") {
-        shiftDown = false;
-    }
-}, true);
