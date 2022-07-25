cf2f137081e305cfb28308b5cebb15a9eaf01a9f
Elliott DuCharme <ellduc4@gmail.com>
2022-07-25 17:12:45 -0500

    Fix issue with Memories where it could load items from the wrong day.
    E.g., picking July 1 but loading items on July 11 too

--- PSS/PSS/Backend/Connection.cs
+++ PSS/PSS/Backend/Connection.cs
@@ -873,7 +873,7 @@ namespace PSS.Backend
             try
             {
                 Open();
-                using NpgsqlCommand cmd = new($"SELECT path, date_taken, starred, uuid, thumbnail FROM media WHERE CAST(date_taken as TEXT) LIKE '%{month}-{day}%' ORDER BY date_taken DESC", connection);
+                using NpgsqlCommand cmd = new($"SELECT path, date_taken, starred, uuid, thumbnail FROM media WHERE CAST(date_taken as TEXT) LIKE '%{month}-{day} %' ORDER BY date_taken DESC", connection);
                 using NpgsqlDataReader r = cmd.ExecuteReader();
                 while (r.Read()) memories.Add(new MediaRow(r.GetString(0), r.GetDateTime(1), r.GetBoolean(2), r.GetGuid(3), r.IsDBNull(4) ? null : r.GetString(4)));
             }
