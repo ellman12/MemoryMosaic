7245e9a08d7a6f6e13edd8e1a55a9890c46760f8
Elliott <ellduc4@gmail.com>
2023-01-03 16:38:06 -0600

    Move UploadFile to and rename to ImportFile.cs

--- PSS/PSS/Backend/Connection.cs
+++ PSS/PSS/Backend/Connection.cs
@@ -124,37 +124,6 @@ namespace PSS.Backend
             }
         }
 
-        ///Represents an item that is being uploaded.
-        public class UploadFile
-        {
-            ///The original (or new if changed by user) filename of this item.
-            public string filename;
-
-            ///The file extension of the item.
-            public string extension;
-
-            ///The path relative to pss_upload of this item.
-            public string shortPath;
-
-            ///The absolute path to the file to upload.
-            public string fullPath;
-            
-            ///null for images, otherwise a base64 string for video files.
-            public string thumbnail;
-            
-            ///If this item is already in pss_library.
-            public bool alreadyInLib;
-            
-            ///The date and time this image or video was captured. null if this item doesnt have any.
-            public DateTime? dateTaken;
-            
-            ///Where the date taken data came from (Filename, Metadata, or None).
-            public D.DateTakenSrc dateTakenSrc;
-
-            ///The uuid of the item, which will be added to the database upon upload.
-            public Guid uuid;
-        }
-
         public static void Open()
         {
             if (connection.State == ConnectionState.Closed)
---
+++ PSS/PSS/Backend/ImportFile.cs
@@ -0,0 +1,32 @@
+﻿namespace PSS.Backend;
+
+///Represents an item that is pending uploading in Import.razor.
+public class ImportFile
+{
+	///The original (or new if changed by user) filename of this item.
+	public string filename;
+
+	///The file extension of the item.
+	public string extension;
+
+	///The path relative to pss_upload of this item.
+	public string shortPath;
+
+	///The absolute path to the file to upload.
+	public string fullPath;
+
+	///null for images, otherwise a base64 string for video files.
+	public string thumbnail;
+
+	///If this item is already in pss_library.
+	public bool alreadyInLib;
+
+	///The date and time this image or video was captured. null if this item doesnt have any.
+	public DateTime? dateTaken;
+
+	///Where the date taken data came from (Filename, Metadata, or None).
+	public D.DateTakenSrc dateTakenSrc;
+
+	///The uuid of the item, which will be added to the database upon upload.
+	public Guid uuid;
+}
