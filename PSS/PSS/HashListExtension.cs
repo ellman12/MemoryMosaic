8150bc2eda3ad8ece6c9d99e0d8f1ef8ff377109
Elliott <ellduc4@gmail.com>
2023-01-21 15:29:56 -0600

    Add HashListExtension.cs

---
+++ PSS/PSS/HashListExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace PSS;
+
+public static class HashListExtension
+{
+	public static HashList<T> ToHashList<T>(this IEnumerable<T> enumerable) => new(enumerable);
+}
