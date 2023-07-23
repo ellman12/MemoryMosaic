92cfd5bb35833d93724052b05e83d7ebabc7989d
Elliott <ellduc4@gmail.com>
2023-07-23 17:17:00 -0500

    Add JS funcs to disable/enable scrolling of page. CS close/confirm buttons now use these

--- MemoryMosaic/wwwroot/MM.js
+++ MemoryMosaic/wwwroot/MM.js
@@ -70,3 +70,13 @@ function toggleBackgroundScrolling()
 {
     document.body.style.overflow = document.body.style.overflow === 'hidden' ? '' : 'hidden';
 }
+
+function disableBackgroundScrolling()
+{
+    document.body.style.overflow = '';
+}
+
+function enableBackgroundScrolling()
+{
+    document.body.style.overflow = 'hidden';
+}
