dbaae8c8bb3bb68a147f37269cc36399eea76d20
Elliott <ellduc4@gmail.com>
2023-07-26 08:53:11 -0500

    Add MemoryMosaic.Classes to Program's global usings

--- MemoryMosaic/Program.cs
+++ MemoryMosaic/Program.cs
@@ -11,6 +11,7 @@ global using System.Threading.Tasks;
 global using MemoryMosaic.Backend;
 global using MemoryMosaic.Backend.Enums;
 global using MemoryMosaic.Backend.Records;
+global using MemoryMosaic.Classes;
 global using Microsoft.VisualBasic.FileIO;
 global using C = MemoryMosaic.Backend.Connection;
 global using D = DateTakenExtractor.DateTakenExtractor;
