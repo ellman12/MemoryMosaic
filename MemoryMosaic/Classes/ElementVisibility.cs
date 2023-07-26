1e7b0f05eb6b5a3391f2c386e7660af6ea621840
Elliott <ellduc4@gmail.com>
2023-07-26 15:56:24 -0500

    Create ElementVisibility.ChangeStates()

--- MemoryMosaic/Classes/ElementVisibility.cs
+++ MemoryMosaic/Classes/ElementVisibility.cs
@@ -25,6 +25,13 @@ public sealed class ElementVisibility
 	public void Disable() => ChangeState(false);
 	public void Enable() => ChangeState(true);
 
+	///Sets the states of multiple items to a new state.
+	public static void ChangeStates(bool newState, params ElementVisibility[] visibilities)
+	{
+		foreach (var visibility in visibilities)
+			visibility.ChangeState(newState);
+	}
+
 	private void ChangeState(bool newState)
 	{
 		Visible = newState;
