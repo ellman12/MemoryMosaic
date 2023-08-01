bd6f327222ddd6683998b1f67b58f6b6d3cac998
Elliott <ellduc4@gmail.com>
2023-08-01 16:57:14 -0500

    Start Button class, and IconButton/TextButton components

---
+++ MemoryMosaic/Shared/Buttons/Button.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace MemoryMosaic.Shared.Buttons;
+
+public abstract class Button : Component
+{
+	[Parameter, EditorRequired] public string Icon { get; set; } = null!;
+	
+	[Parameter, EditorRequired] public Action OnClick { get; set; } = null!;
+}
