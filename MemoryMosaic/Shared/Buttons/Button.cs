addbd193e340e59bb154da69d455c0fc89829001
Elliott <ellduc4@gmail.com>
2023-08-02 15:43:47 -0500

    Add Title parameter to buttons

--- MemoryMosaic/Shared/Buttons/Button.cs
+++ MemoryMosaic/Shared/Buttons/Button.cs
@@ -7,4 +7,6 @@ public abstract class Button : Component
 	[Parameter, EditorRequired] public string Icon { get; set; } = null!;
 	
 	[Parameter, EditorRequired] public Action OnClick { get; set; } = null!;
+	
+	[Parameter] public string? Title { get; set; }
 }
