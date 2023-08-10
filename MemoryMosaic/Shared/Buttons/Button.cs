23b7b7a58376ea89f7d0d5f31f54e1fbbe2372e6
Elliott <ellduc4@gmail.com>
2023-08-10 14:57:59 -0500

    Refactor IconButton and IconTextButton to inherit from ActionButton, which inherits from Button

--- MemoryMosaic/Shared/Buttons/Button.cs
+++ MemoryMosaic/Shared/Buttons/Button.cs
@@ -6,8 +6,6 @@ public abstract class Button : Component
 {
 	[Parameter, EditorRequired] public string Icon { get; set; } = null!;
 	
-	[Parameter, EditorRequired] public Action OnClick { get; set; } = null!;
-
 	[Parameter] public string? IconStyle { get; set; }
 	
 	[Parameter] public string? Title { get; set; }
