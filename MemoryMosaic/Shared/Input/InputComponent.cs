3e28fcdcdce871712a579f1a2010065537954324
Elliott <ellduc4@gmail.com>
2023-08-21 20:14:51 -0500

    Create AlphanumericInput<T>. Inherits from InputComponent<T>. Adjust field declarations.

--- MemoryMosaic/Shared/Input/InputComponent.cs
+++ MemoryMosaic/Shared/Input/InputComponent.cs
@@ -5,29 +5,17 @@ namespace MemoryMosaic.Shared.Input;
 
 public class InputComponent<T> : Component
 {
-	[Parameter]
-    public T? Input { get; set; }
+	[Parameter] public T? Input { get; set; }
 
-	[Parameter]
-    public EventCallback<T?> InputChanged { get; set; }
+	[Parameter] public EventCallback<T?> InputChanged { get; set; }
 
-	[Parameter]
-    public string? Placeholder { get; set; }
+	[Parameter] public bool Disabled { get; set; }
 
-	[Parameter]
-	public int FontSize { get; set; } = 16;
+	[Parameter] public int FontSize { get; set; } = 16;
 
-	[Parameter]
-	public string Width { get; set; } = "200px";
+	[Parameter] public Action<KeyboardEventArgs>? OnKeyDown { get; set; }
 
-	[Parameter]
-	public bool Disabled { get; set; }
-
-	[Parameter]
-	public Action<KeyboardEventArgs>? OnKeyDown { get; set; }
-
-	[Parameter]
-	public Action? OnFocusOut { get; set; }
+	[Parameter] public Action? OnFocusOut { get; set; }
 
 	public async void Focus() => await input.FocusAsync();
 
@@ -38,9 +26,4 @@ public class InputComponent<T> : Component
 	protected void HandleKeyDown(KeyboardEventArgs e) => OnKeyDown?.Invoke(e);
 
 	protected void HandleFocusOut() => OnFocusOut?.Invoke();
-
-	protected override void OnInitialized()
-	{
-		Width = Width == "available" ? "available; width: -moz-available; width: -webkit-fill-available;" : Width;
-	}
 }
