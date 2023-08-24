31cd82482aba26b5a9cd0ce4be8e97a0dbc0fb19
Elliott <ellduc4@gmail.com>
2023-08-24 16:40:40 -0500

    Condense Program.CreateHostBuilder down to a single line.

--- MemoryMosaic/Program.cs
+++ MemoryMosaic/Program.cs
@@ -46,10 +46,5 @@ public class Program
         CreateHostBuilder(args).Build().Run();
     }
 
-    private static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
 }
