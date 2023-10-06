a6b14b70adabad1d92bcdb85a4d582843a239751
Elliott <ellduc4@gmail.com>
2023-10-06 17:54:12 -0500

    Simplify event handlers and fix indents

--- MemoryMosaic/wwwroot/VideoPlayer.js
+++ MemoryMosaic/wwwroot/VideoPlayer.js
@@ -1,22 +1,24 @@
-﻿//TODO: find a not dumb way to only include this file in VideoPlayer, or LCV/Import.
+//TODO: find a not dumb way to only include this file in VideoPlayer, or LCV/Import.
 
 //Dumb hack to ensure getElementsByTagName() actually finds the video element.
 function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 let video; //HTMLVideoElement
 
 window.onload = async () => {
-    await delay(40);
-    video = document.getElementsByTagName("video")[0];
+	await delay(40);
+	video = document.getElementsByTagName("video")[0];
 
-    video.addEventListener("contextmenu", e => e.preventDefault());
+	video.oncontextmenu = e => e.preventDefault()
 
-    video.addEventListener("click", () => {
-        if (video.paused)
-            video.play();
-        else
-            video.pause();
-    });
+	video.onclick = () => {
+		togglePlaying()
+	}
+function togglePlaying() {
+	if (video.paused)
+		video.play();
+	else
+		video.pause();
 }
