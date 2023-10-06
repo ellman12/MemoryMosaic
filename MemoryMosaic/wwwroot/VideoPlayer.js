7c00477afc8fd1be942f371fc9cbc6c250df880a
Elliott <ellduc4@gmail.com>
2023-10-06 16:23:59 -0500

    Start work on VideoPlayer

---
+++ MemoryMosaic/wwwroot/VideoPlayer.js
@@ -0,0 +1,15 @@
+﻿//TODO: find a not dumb way to only include this file in VideoPlayer, or LCV/Import.
+
+//Dumb hack to ensure getElementsByTagName() actually finds the video element.
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let video; //HTMLVideoElement
+
+window.onload = async () => {
+    await delay(40);
+    video = document.getElementsByTagName("video")[0];
+    
+    video.addEventListener('contextmenu', e => e.preventDefault());
+}
