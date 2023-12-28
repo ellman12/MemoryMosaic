ab391c725d8c1d0612a2dfe5641cab7e7bfcf7c0
Elliott <ellduc4@gmail.com>
2023-12-28 17:54:33 -0600

    Create LibraryItem constructor for converting an ImportItem

--- MemoryMosaic/Models/LibraryItem.cs
+++ MemoryMosaic/Models/LibraryItem.cs
@@ -26,6 +26,20 @@ public sealed class LibraryItem : Media
 	///Creates a new LibraryItem from a query of this form: path, id, date_taken, date_added, starred, description, date_deleted, thumbnail
 	public LibraryItem(NpgsqlDataReader reader) : this(reader.GetString(0), reader.GetGuid(1), reader.TryGetDateTime(2), reader.GetDateTime(3), reader.GetBoolean(4), reader.IsDBNull(5) ? null : reader.GetString(5), reader.IsDBNull(6) ? null : reader.GetDateTime(6), reader.GetString(7)) {}
 
+	///Used in Import.AddItems() for converting an ImportItem into a LibraryItem.
+	public LibraryItem(ImportItem importItem)
+	{
+		Path = importItem.DestinationPath;
+		Id = importItem.Id;
+		DateTaken = importItem.SelectedDateTaken;
+		DateAdded = DateTime.Now;
+		Starred = importItem.Starred;
+		Description = importItem.Description;
+		DateDeleted = null;
+		Thumbnail = importItem.Thumbnail;
+		Video = F.SupportedVideoExts.Contains(P.GetExtension(Path).ToLower());
+	}
+	
 	public override string RequestPath => "mm_library";
 
 	public override string FullPath => P.Combine(S.LibFolderPath, Path);
