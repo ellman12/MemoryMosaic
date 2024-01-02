04b239cc4749c8c68a4fac913af3054ad8fe8434
Elliott <ellduc4@gmail.com>
2024-01-02 14:45:18 -0600

    Create ItemDisplay base class for display components

---
+++ MemoryMosaic/Shared/Import/ItemDisplay.cs
@@ -0,0 +1,18 @@
+﻿namespace MemoryMosaic.Shared.Import;
+
+using Microsoft.AspNetCore.Components;
+using Pages;
+
+///Base class for ImportItemDisplay and LibraryItemDisplay.
+public abstract class ItemDisplay<T> : Component where T : Media
+{
+	[Parameter, EditorRequired] public required Import Import { get; set; }
+	
+	[Parameter, EditorRequired] public required T Item { get; set; }
+	
+	[Parameter, EditorRequired] public required int Index { get; set; }
+	
+	[Parameter, EditorRequired] public required string TextClass { get; set; }
+	
+	protected string PathWidth => Import.PathWidth == "full" || TextClass == "error" ? "full" : "short";
+}
