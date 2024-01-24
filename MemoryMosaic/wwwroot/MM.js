efe10fa0f67a73498b03ea03dff5bc996df84d8a
Elliott <ellduc4@gmail.com>
2024-01-24 10:45:18 -0600

    Remove lots of old JS functions that are no longer used

--- MemoryMosaic/wwwroot/MM.js
+++ MemoryMosaic/wwwroot/MM.js
@@ -1,53 +1,11 @@
-function openEdit(index) {
-    document.getElementById('text ' + index).style.display = 'inline';
-    document.getElementById('close ' + index).style.display = 'inline';
-    document.getElementById('save ' + index).style.display = 'inline';
+function toggleBackgroundScrolling() {
+	document.body.style.overflow = document.body.style.overflow === 'hidden' ? '' : 'hidden';
 }
 
-function closeEdit(index) {
-    document.getElementById('text ' + index).style.display = 'none';
-    document.getElementById('close ' + index).style.display = 'none';
-    document.getElementById('save ' + index).style.display = 'none';
+function enableBackgroundScrolling() {
+	document.body.style.overflow = '';
 }
 
-function toggleSelect(index) {
-    let select = document.getElementById('select ' + index);
-    if (select.style.display === 'none') {
-        select.style.display = 'inline';
-    } else {
-        select.style.display = 'none';
-    }
-}
-
-function openSelect(index) {
-    document.getElementById('select ' + index).style.display = 'inline';
-}
-
-function closeSelect(index) {
-    document.getElementById('select ' + index).style.display = 'none';
-}
-
-function openDtPicker(index)
-{
-    document.getElementById('dtPicker ' + index).style.display = 'inline';
-}
-
-function closeDtPicker(index)
-{
-    document.getElementById('dtPicker ' + index).style.display = 'none';
-}
-
-function toggleBackgroundScrolling()
-{
-    document.body.style.overflow = document.body.style.overflow === 'hidden' ? '' : 'hidden';
-}
-
-function enableBackgroundScrolling()
-{
-    document.body.style.overflow = '';
-}
-
-function disableBackgroundScrolling()
-{
-    document.body.style.overflow = 'hidden';
+function disableBackgroundScrolling() {
+	document.body.style.overflow = 'hidden';
 }
