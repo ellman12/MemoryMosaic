144f830b331fac7700cf688ff60876b8c0a4a6dd
Elliott DuCharme <ellduc4@gmail.com>
2024-09-04 20:40:38 -0500

    Create CoordinateExtractor.cs

---
+++ MemoryMosaic/Backend/CoordinateExtractor.cs
@@ -0,0 +1,42 @@
+﻿namespace MemoryMosaic.Backend;
+
+///Facilitates reading and writing GPS coordinates for items.
+public static class CoordinateExtractor
+{
+	///Given a file path, use ExifTool to read the coordinates of the file.
+	public static (double, double)? ReadCoordinates(string fullPath)
+	{
+		using Process p = new();
+		p.StartInfo = new ProcessStartInfo
+		{
+			FileName = "exiftool",
+			Arguments = $"\"{fullPath}\" -GPSlatitude -GPSlongitude -s3 -c \"%+6f\"",
+			CreateNoWindow = true,
+			RedirectStandardInput = true,
+			RedirectStandardOutput = true, //Necessary for reading output of ExifTool.
+			WindowStyle = ProcessWindowStyle.Hidden
+		};
+		p.Start();
+		p.WaitForExit();
+
+		if (Double.TryParse(p.StandardOutput.ReadLine(), out var latitude) && Double.TryParse(p.StandardOutput.ReadLine(), out var longitude))
+			return new (latitude, longitude);
+
+		return null;
+	}
+
+	///Given a file path, use ExifTool to update the coordinates of the file.
+	public static void UpdateCoordinates(string fullPath, double? latitude, double? longitude)
+	{
+		using Process p = new();
+		p.StartInfo = new ProcessStartInfo
+		{
+			FileName = "exiftool",
+			Arguments = $"\"{fullPath}\" -GPSlatitude={(latitude == null ? "" : latitude)} -GPSlongitude={(longitude == null ? "" : longitude)}",
+			CreateNoWindow = true,
+			WindowStyle = ProcessWindowStyle.Hidden
+		};
+		p.Start();
+		p.WaitForExit();
+	}
+}
