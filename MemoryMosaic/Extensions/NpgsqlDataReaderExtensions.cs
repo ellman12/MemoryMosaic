470c7c3e259e8d40096658409c7ad544de57e542
Elliott DuCharme <ellduc4@gmail.com>
2024-09-05 21:31:46 -0500

    Create TryGetDouble()

--- MemoryMosaic/Extensions/NpgsqlDataReaderExtensions.cs
+++ MemoryMosaic/Extensions/NpgsqlDataReaderExtensions.cs
@@ -2,13 +2,9 @@
 
 public static class NpgsqlDataReaderExtensions
 {
-	public static DateTime? TryGetDateTime(this NpgsqlDataReader reader, int ordinal)
-	{
-		return reader.IsDBNull(ordinal) ? null : reader.GetDateTime(ordinal);
-	}
+	public static DateTime? TryGetDateTime(this NpgsqlDataReader reader, int ordinal) => reader.IsDBNull(ordinal) ? null : reader.GetDateTime(ordinal);
 
-	public static string? TryGetString(this NpgsqlDataReader reader, int ordinal)
-	{
-		return reader.IsDBNull(ordinal) ? null : reader.GetString(ordinal);
-	}
+	public static string? TryGetString(this NpgsqlDataReader reader, int ordinal) => reader.IsDBNull(ordinal) ? null : reader.GetString(ordinal);
+	
+	public static double? TryGetDouble(this NpgsqlDataReader reader, int ordinal) => reader.IsDBNull(ordinal) ? null : reader.GetDouble(ordinal);
 }
